c1f2dd64699308653831d37b3ffc1f7f48b29a12
monu <sawlanimohnish@gmail.com>
2024-06-30 18:43:29 +0530

    x-dictionary implemented

---
+++ src/Dictionary/Dictionary.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const data = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+const Dictionary = () => {
+  const input = useRef(null);
+  const [result, setResult] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = input.current.value;
+
+    const result = data.find(
+      (item) => item.word.toLowerCase() === inputValue.toLowerCase()
+    );
+    if (result) {
+      setResult(result.meaning);
+    } else {
+      setResult("Word not found in the dictionary");
+    }
+  };
+
+  return (
+    <>
+      <div style={{ margin: "1rem", textAlign: "left" }}>
+        <h1>Dictionary App</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="input"></label>
+          <input name="input" type="text" ref={input} />
+          <button type="submit">Search</button>
+        </form>
+        <p>
+          <b>Definition:</b>
+        </p>
+        <p>{result}</p>
+      </div>
+    </>
+  );
+};
+export default Dictionary;
